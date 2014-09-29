ad117450723eccc2ff10f00d6e38341a45083af2
BelhadjTaslim <taslim.belhadj.sio@gmail.com>
2014-09-29 15:46:54 +0200

    Classes Revision, Revision[getNumero]

--- Revision.java
+++ Revision.java
@@ -1,9 +1,15 @@
 public class Revision{
 	private String imm ;
 	private int numRevision ;
-}
 
-public Revision(imm, numRevision){
-	this.imm = imm ;
-	this.numRevision = numRevision ;
+
+	public Revision(imm, numRevision){
+		this.imm = imm ;
+		this.numRevision = numRevision ;
+	}
+	public getNumero(){
+		return this.numRevision ;
+
+	}
+
 }
